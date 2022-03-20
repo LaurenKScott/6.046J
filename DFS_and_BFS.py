8f6b19ee437ebe1d5e07629dc63ae8f68b1d2c4a
LaurenKScott <lkscottcom@gmail.com>
2022-03-20 00:52:07 -0400

    mod dfs fixed

--- DFS_and_BFS.py
+++ DFS_and_BFS.py
@@ -1,11 +1,12 @@
 '''
 Depth-first search and Breadth-first search
 '''
-
-# represent weights with nodes ds
+from re import L
+from Data_Structures import stacks_and_queues as sq
+# represent weights with vertexs ds
 # profit is a positive int, representing potential profit of a site
-# name is a string representing the name of the node (for easy visualization)
-class Node:
+# name is a string representing the name of the vertex (for easy visualization)
+class Vertex:
     def __init__(self, profit, name= None):
         self.name = name
         self.profit = profit
@@ -15,63 +16,114 @@ class Node:
     def get_name(self):
         return self.name
 
-A = Node(1000,'A')
-B = Node(800, 'B')
-C = Node(350, 'C')
-D = Node(400, 'D')
-E = Node(1300, 'E')
-F = Node(200, 'F')
-G = Node(700, 'G')
-H = Node(1400, 'H')
-I = Node(2900, 'I')
-J = Node(300, 'J')
-K = Node(850, 'K')
+A = Vertex(1000,'A')
+B = Vertex(800, 'B')
+C = Vertex(350, 'C')
+D = Vertex(400, 'D')
+E = Vertex(1300, 'E')
+F = Vertex(200, 'F')
+G = Vertex(700, 'G')
+H = Vertex(1400, 'H')
+I = Vertex(2900, 'I')
+J = Vertex(300, 'J')
+K = Vertex(850, 'K')
 
-# graph is a dictionary of Node object pointers
+# graph is a dictionary of vertex object pointers
 graph = {A:[C, D], B:[C], C:[A, B, F, G], D:[A, E],
  E:[D, H], F:[C, I], G: [C], H:[E, K], I:[F], J:[K], K:[H,J]}
 
 '''
 DEPTH FIRST SEARCH
 '''
-# visited is an array of previously explored nodes
+# visited is an array of previously explored vertexs
 # graph is a dict of adjacency lists (represents undir graph)
-# node is node object
-def DFS(visited, graph, node):
-    if node not in visited:
-        visited.append(node)
-        for neighbor in graph[node]:
-            DFS(visited, graph, neighbor)
+# vertex object
+def DFS(graph, vertex):
+    visited = []
+    stack = sq.Stack()
+    stack.push(vertex)
+    while not stack.isEmpty():
+        to_visit = stack.pop()
+        if to_visit not in visited:
+            visited.append(to_visit)
+            for neighbor in graph[to_visit]:
+                if neighbor not in visited:
+                    stack.push(neighbor)
     return visited
 
 '''
 BREADTH FIRST SEARCH
 '''
-def BFS(graph, node):
+def BFS(graph, vertex):
     visited = []
-    queue= []
-    visited.append(node)
-    queue.append(node)
+    queue= sq.Queue()
+    visited.append(vertex)
+    queue.enqueue(vertex)
     while queue:
-        to_vis = queue.pop(0)
+        to_vis = queue.dequeue()
         for neighbor in graph[to_vis]:
             if neighbor not in visited:
-                queue.append(neighbor)
+                queue.enqueue(neighbor)
                 visited.append(neighbor)
     return visited
 
 def print_path(path):
-    for node in path[:-1]:
-        print(node.get_name(), end=" --> ")
+    for vertex in path[:-1]:
+        print(vertex.get_name(), end="  ")
     print(path[-1].get_name())
 
 def path_weight(visited):
     sum = 0
-    for node in visited:
-        sum += node.get_profit()
+    for vertex in visited:
+        sum += vertex.get_profit()
     return sum
 
-node = A 
-visited= []
-print_path(DFS(visited, graph, node))
-print_path(BFS(graph, node))
+def mod_DFS(graph, vertex):
+    visited = []
+    skip = []
+    stack = sq.Stack()
+    stack.push(vertex)
+    while not stack.isEmpty():
+        to_visit = stack.pop()
+        if to_visit not in visited and to_visit not in skip:
+            visited.append(to_visit)
+            for neighbor in graph[to_visit]:
+                if neighbor not in visited and neighbor not in skip:
+                    skip.append(neighbor)
+                if neighbor in skip:
+                    for n in graph[neighbor]:
+                        if n not in visited and n not in skip:
+                            stack.push(n)
+    return visited
+#nod = A 
+#visited= []
+#print_path(DFS(visited, graph, nod))
+#print()
+# print_path(BFS(graph, vertex))
+
+store_graph = [{'vertex': vertex, 'name': vertex.get_name(), 'profit': vertex.get_profit()} 
+    for vertex in graph.keys()]
+def sort_vertex(graph):
+    return graph['profit']
+store_graph.sort(key=sort_vertex, reverse=True)
+final_graph = []
+for dict in store_graph:
+    final_graph.append(dict['vertex'])
+
+'''
+we want to memoize paths that have already been explored via DFS
+
+'''
+vis = []
+weight = {}
+for vertex in final_graph:
+    if vertex not in vis:
+        path = mod_DFS(graph, vertex)
+        strpath = print_path(path)
+        weight[strpath] =  path_weight(path)
+        for node in path:
+            vis.append(node)
+
+print(weight)
+
+        
