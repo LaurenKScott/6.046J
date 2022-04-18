6d8353d4f83ed4f3f2dfb0ae25048c7618bb5820
LaurenKScott <lkscottcom@gmail.com>
2022-04-18 10:46:43 -0400

    lup_solve

--- matrix.py
+++ matrix.py
@@ -10,12 +10,7 @@ Systems of equations, polynomial operations
 Encryption, machine learning
 '''
 # Matrix as a list of lists (array of arrays whatever)
-emx = [
-    [2, 3, 1, 5],
-    [6, 13, 5, 19],
-    [2, 19, 10, 23],
-    [4, 10, 11, 31]
-]
+
 
 '''
 # LUP Decomposition of a matrix, A
@@ -36,8 +31,8 @@ def get_L(A):
     for j in range(n):
         # MAIN DIAGONAL of A is A[x][x] for all x in n
         # populate main diagonal with ones (if x == j, we are on main bc A[j][x] == A[x][x])
-        # else, if x < j, we are BELOW the diagonal. so populate with A[j][x]
-        # everything else (ABOVE diagonal) is 0
+        # everything ABOVE diagonal is 0 x>j means column > row so to the right or above main
+        # else None (to fill in later w/ LU decomp)
         row = [1 if x==j else (0 if x > j else None)  for x in range(n)]
         L.append(row)
     return L
@@ -46,12 +41,13 @@ def get_U(A):
     n = len(A)
     U = []
     for j in range(n):
+        # 0s below the diagonal 
         row = [None if x > j else 0 for x in range(n)]
         U.append(row)
     return U
 
 # REQUIRES SQUARE MATRIX INPUT, assume list of list representation      
-def LUP_Decomp(A):
+def LU_Decomp(A):
     # pivot starts at a11 downto ann
     # v is remaining column values
     # omega_t is remaining row values
@@ -71,54 +67,54 @@ def LUP_Decomp(A):
                 A[i][j] = A[i][j] - (L[i][k] * U[k][j])
     return L, U
     
-   
-print(LUP_Decomp(emx))
+def LUP_Decomp(A):
+    n = len(A)
+    pi = [x for x in range(n)]
+    for k in range(n): # go row by row. O(n)
+        p = 0
+        for i in range(k, n): # O(n-k)
+            if abs(A[i][k]) >  p: #we want to choose a large p to pivot on
+                p = abs(A[i][k])
+                k_pr = i #k_pr will be the row number we swap
+        if p == 0: # all potential pivots = 0, determinant 0 --> singular
+            raise Exception("Singular matrix") 
+        # swap row places in permutation array
+        pi[k], pi[k_pr] = pi[k_pr], pi[k] 
+        # now swap each entry in row k w row k_pr
+        for i in range(n): 
+            A[k][i], A[k_pr][i] = A[k_pr][i], A[k][i]
+        # create v vector s.t. v = v/pivot value
+        for i in range(k+1, n):
+            A[i][k] = A[i][k] / A[k][k]
+            for j in range(k+1, n):
+                A[i][j] = (A[i][j] - (A[i][k]*A[k][j]))
+    return pi, A
 
-# From Ax = b, PA = LU, we have PAx=Pb ==> LUx = Pb
-# A = P(inv)LU
-# L, U are matrices (see above comments for definitions)
-# b is the resulting vector from computing Ax
-# pi is an array representation of P 
-def LUP_Solve(L, U, pi, b):
-    #n is number of rows
-    n = len(L)
-    x = [] # new length n vector
-    y = []
-    # for each row in L, calculate y using forward substitution
+#PA=LU so PAx = LUx = Pb
+def LUP_Solve(pi, A, b):
+    n = len(A)
+    x = [None] * n
+    # y = U*x 
+    y = [None] * n
     for i in range(n):
-        # Let Ux = y, so that Ly = Pb
-        # Forward substition: Pb has b*pi_i as its ith elt
-        # y[i] = b*pi[i] - SUM(L[i][j]*y[j]) <-- sum from j=1 to i-1
-        y[i] = b[pi]
+        y[i] = b[pi[i]]
         for j in range(i):
-            y[i] -= (L[i][j])*y[j]
-    print(y)
-    # Now use backwards substitution to solve for x
-    for i in range(n, 0, -1):
-        print(i, 'x')
+            y[i] -= A[i][j]*y[j]
+    for i in range(n-1, -1, -1):
+        x[i] = y[i]
+        for j in range(i+1,n):
+            x[i] -= (A[i][j]*x[j])
+        x[i] = round(x[i]/A[i][i], 2)
+    return x
 
-#pass a list of lists
-class Matrix:
-    def __init__(self, rows, cols):
-        # rows = int number of rows
-        self.rows = rows
-        # cols = int number of columns
-        self.cols = cols
-        # initialize an empty (0x0) matrix
-        self.mat = []
-        for i in range(rows):
-            # get row by row input from the user, strip any whitespaces
-            # ignore any inputs outside of column range
-            row_input = input("Enter one integer per entry, separated by space ").strip().split()[:cols]
-            # make that row into a list of integers (assume int input)
-            ri = list(map(int, row_input))
-            # append it to our list-of-lists matrix
-            self.mat.append(ri)
-    def get_mat(self):
-        return self.mat
-    def get_entry(self, i, j):
-        if i < self.rows and j < self.cols:
-            return self.mat[i][j]
-        else:
-            return False
-
+Apr = [[1., 5., 4.],[2.,0.,3.],[5.,8.,2.]]
+#bA = [12.,9.,5.]
+Bpr = [[1., 2., 0.],[3., 4., 4.],[5., 6., 3.]]
+bB = [3., 7., 8.]
+decompB = LUP_Decomp(Bpr)
+piB = decompB[0]
+mB = decompB[1]
+#decomp_result = LUP_Decomp(Apr)
+#pi = decomp_result[0]
+#mA = decomp_result[1]
+print(LUP_Solve(piB, mB, bB))
