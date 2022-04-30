ad0839ff8f8b3a433ce67032cfa376331bcb62eb
LaurenKScott <lkscottcom@gmail.com>
2022-04-30 16:48:05 -0400

    working on radix subroutine

--- linear_sort.py
+++ linear_sort.py
@@ -35,6 +35,34 @@ What's so great about COUNTING SORT?
 it runs in O(n+k). that means, if k = O(n), then the entire sort
 runs in LINEAR TIME. but how do we know k? sometimes we have an input 
 array with a well-defined max (example: range of test scores)
+
+STABLE SORT:
+the elements that occur first in A, will occur first in B also
+this is why decrementing C is so important
 '''
 print(count_sort(A, 5))
+
+
+def count_sort_dig(A, digit, r):
+    B = [None] * len(A)
+    C = [0] * r
+    for i in range(len(A)):
+        digAi = (A[i] // (r**digit)) % r
+        C[digAi] += 1
+        # now C[digAi] has # of occurences of digAi in A
+    for j in range(1, r):
+        C[j] += C[j-1]
+        # now C[i] contains # elements in A less than or equal to i
+    # working backwards from len(A) to 0
+    for m in range(len(A)-1, -1, -1):
+        digAi = (A[m]//(r**digit)) % r
+        C[digAi] -= 1
+        B[C[digAi]] = A[m]
+    return B
+
+print(count_sort_dig(A, 1, 5))
+# A is the input array, r is a chosen radix
+def radix_sort(A, r):
+    for i in range(r):
+        pass
 
